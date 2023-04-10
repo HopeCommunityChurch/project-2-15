8d4c7f5b6ff3e770a1b2fafa72d45001649270ee
Jonny <jonny.covert@gmail.com>
2023-04-10 18:50:04 -0400

    Got some react stuff working.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,3 +1,8 @@
 import {body} from "./jonathanMeme/index.tsx";
+import ReactDOM from "react-dom";
 console.log("hello world");
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1 className={body} >Hello, world!</h1>);
+
 console.log(body);
