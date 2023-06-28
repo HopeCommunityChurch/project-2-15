21e38111137362545dd7c415e3d52dd332acee66
Jonny <jonny@p215.church>
2023-06-27 23:20:18 -0400

    Use unstable version of colmena

--- ops/flake.nix
+++ ops/flake.nix
@@ -2,23 +2,25 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    colmena.url = "github:zhaofengli/colmena/release-0.4.x";
+    colmena.url = "github:zhaofengli/colmena";
   };
 
   outputs = inputs@{ self, colmena, nixpkgs, flake-parts }: flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem = {pkgs, ...} :
+      perSystem = {pkgs, system, ...} :
+        let colmena-pkg = colmena.defaultPackage.${system};
+        in
         {
           apps.default = {
             type = "app";
             program = toString (pkgs.writers.writeBash "apply" ''
               cd network \
-              && ${colmena}/bin/colmena build
+              && ${colmena-pkg}/bin/colmena build
             '');
           };
           devShells.default = pkgs.mkShell {
             buildInputs = [
-              colmena
+              colmena-pkg
             ];
           };
         };
