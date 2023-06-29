bb98b20fcb027bbb971ea1f78cb0042012ac564f
Jonny <jonny@p215.church>
2023-06-28 22:40:54 -0400

    Little simple haskell webserver.

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -1,5 +1,11 @@
 module Main where
 
+import Prelude hiding (get)
+import Web.Scotty
+
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
+main =
+  scotty 3000 $
+    get "/:word" $ do
+        beam <- param "word"
+        html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
