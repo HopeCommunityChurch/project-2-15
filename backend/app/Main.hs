8abc3797e3bb7747e9ea17ebe4ceeddb78a6a2d3
Jonny <jonny.covert@gmail.com>
2023-06-28 22:54:42 -0400

    For some reason I can't deploy?

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -5,7 +5,9 @@ import Web.Scotty
 
 main :: IO ()
 main =
-  scotty 3000 $
+  scotty 3000 $ do
     get "/:word" $ do
         beam <- param "word"
         html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+    get "/" $ do
+        html "<h1>Scotty, me up!</h1>"
