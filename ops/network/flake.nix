7773678c8ae7bcfe6ce876978ca445265f31c618
Jonny <jonny@p215.church>
2023-06-28 22:51:13 -0400

    The api server should be running.

--- ops/network/flake.nix
+++ ops/network/flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    backend.url = "path:../../backend/flake.nix";
   };
   outputs = inputs@{ nixpkgs, ... }: {
     colmena = {
@@ -18,12 +19,14 @@
         };
         boot.isContainer = true;
 
-        networking.firewall.allowedTCPPorts = [ 22 80 443 ];
+        networking.firewall.allowedTCPPorts = [ 22 80 443 3000 ];
 
         services.do-agent.enable = true;
+
         services.sshd = {
           enable = true;
         };
+
         services.nginx = {
           enable = true;
           virtualHosts.default = {
@@ -31,6 +34,24 @@
             locations."/".return = "200 \"Hello from Nixie!\"";
           };
         };
+
+        systemd.services.backend =
+          let backend = inputs.backend.packages.x86_64-linux.backend;
+          in
+            { description = "p215 backend";
+              after = [ "network.target" ];
+              wantedBy = [ "multi-user.target" ];
+              serviceConfig = {
+                WorkingDirectory = "${backend}/bin/";
+                ExecStart = "${backend}/bin/backend +RTS -M1G -T";
+                Restart = "always";
+                RestartSec = 3;
+                LimitNOFILE = 65536;
+                Environment = [  ]; # env variables here
+              };
+            };
+
+
       };
     };
   };
