a13e7707f70d9c6ab69146b96d39796db5a1c5fa
Jonny <jonny.covert@gmail.com>
2023-07-03 19:10:15 -0400

    Migration stuff.

--- backend/migrations/2023-07-03T04:59:30.sql
+++ backend/migrations/2023-07-03T04:59:30.sql
@@ -1,7 +1,7 @@
-CREATE EXTENSION citext;
+CREATE EXTENSION if not exists citext;
 
 create table if not exists "church"
-  ( "churchId" uuid not null
+  ( "churchId" uuid not null default gen_random_uuid()
   , "name" text not null
   , "created" timestamptz not null default now()
   , PRIMARY KEY ("churchId")
@@ -9,7 +9,7 @@ create table if not exists "church"
 
 
 create table if not exists "user"
-  ( "userId" uuid not null
+  ( "userId" uuid not null default gen_random_uuid()
   , "email" citext not null
   , "image" text
   , "churchId" uuid not null
