eedb19bf2d305e4ecf65f0a41df24ad7b3c30d26
James Hendrie <jhendrie25@gmail.com>
2023-08-12 12:57:58 -0400

    fixed error that made it not run

--- frontend/src/Pages/Studies/studies.tsx
+++ frontend/src/Pages/Studies/studies.tsx
@@ -1,8 +1,7 @@
-
 import { createEffect, createSignal, onMount, For, createResource } from "solid-js";
 import { Button } from "../../Components/Button/Button";
 import { PreLoginTopNav } from "../../Components/PreLoginTopNav/PreLoginTopNav";
-import { match, P } from 'ts-pattern';
+// import { match, P } from 'ts-pattern';
 
 import * as Network from "../../Utils/Network";
 import * as classes from "./styles.module.scss";
@@ -18,56 +17,37 @@ type Study = {
   docs: {}[];
 };
 
-
-async function getStudies() : Promise<Network.NetworkState<Array<Study>>> {
+async function getStudies(): Promise<Network.NetworkState<Array<Study>>> {
   return Network.request("/study");
 }
 
 export function StudiesPage() {
   console.log("test");
-  const [result] = createResource(
-    [],
-    () => getStudies(),
-    { initialValue: {state: "loading"} }
-  )
-
+  const [result] = createResource([], () => getStudies(), { initialValue: { state: "loading" } });
 
   return (
     <>
       <PreLoginTopNav />
       <div>
-      { match(result())
-          .with({state: "loading"}, () =>
-            (<>loading</>)
-          )
-          .with({state: "error"}, ({body}) =>
-            (<>error</>)
-          )
-          .with({state: "success"}, ({body}) =>
-            (<div>
-                <For each={body}>
-                  { (study) => <ViewStudy study={study} /> }
-                </For>
-              </div>
-            )
-          )
-          .with({state: "notloaded"}, () =>
-            (<>not loaded</>)
-          ).exhaustive()
-        }
+        {match(result())
+          .with({ state: "loading" }, () => <>loading</>)
+          .with({ state: "error" }, ({ body }) => <>error</>)
+          .with({ state: "success" }, ({ body }) => (
+            <div>
+              <For each={body}>{(study) => <ViewStudy study={study} />}</For>
+            </div>
+          ))
+          .with({ state: "notloaded" }, () => <>not loaded</>)
+          .exhaustive()}
       </div>
     </>
   );
 }
 
 type ViewStudyProps = {
-  study : Study;
+  study: Study;
 };
 
-function ViewStudy (props : ViewStudyProps) {
-  return (
-    <div>
-      {props.study.name}
-    </div>
-  );
+function ViewStudy(props: ViewStudyProps) {
+  return <div>{props.study.name}</div>;
 }
