f413e8e3f893461e0c0668407035e8a20cfbd7e5
James Hendrie <jhendrie25@gmail.com>
2023-11-17 17:18:41 -0500

    added esc exit to study title input

--- frontend/src/Pages/Study/StudyTopNav/StudyTopNav.tsx
+++ frontend/src/Pages/Study/StudyTopNav/StudyTopNav.tsx
@@ -258,7 +258,7 @@ export function StudyTopNav(props: StudyTopNavProps) {
           }}
           onPaste={handlePaste}
           onKeyDown={(e) => {
-            if (e.key === "Enter" && e.target instanceof HTMLElement) {
+            if ((e.key === "Enter" || e.key === "Escape") && e.target instanceof HTMLElement) {
               e.preventDefault();
               e.target.blur();
             }
