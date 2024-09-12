18274eb96a73d128c92955e762e0ac477ddcbf4b
jonny <jonny.covert@gmail.com>
2024-09-12 00:13:51 -0400

    Fix issue with chrome not getting random stuff.

--- frontend/src/Util.tsx
+++ frontend/src/Util.tsx
@@ -2,7 +2,7 @@
 
 export function getRandomStr(): string {
   const arrayb = new Uint8Array(10);
-  let b = self.crypto.getRandomValues(arrayb);
+  let b = crypto.getRandomValues(arrayb);
   return btoa(b.reduce((a, b) => a + b, ""));
 }
 
