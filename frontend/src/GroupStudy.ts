68e40ea6b2bc0bd8b54ffa935ec268b0d3c46910
jonny <jonny.covert@gmail.com>
2024-10-26 23:08:45 -0400

    Fix small bug.

--- frontend/src/GroupStudy.ts
+++ frontend/src/GroupStudy.ts
@@ -3,48 +3,50 @@ import * as T from "./Types"
 import * as Editor from "./Editor/Editor"
 
 export function init(ws : WS.MyWebsocket) {
-  const groupStudySelector =
-    document.getElementById("groupStudySelector") as HTMLSelectElement ;
-
-  let editor : Editor.P215Editor = null;
-  let currentDocId : T.DocId;
-
-  groupStudySelector.addEventListener("input", () => {
-    loadEditor();
-  });
-
   const splitscreenButton = document.getElementById("splitscreenButton");
-  splitscreenButton.addEventListener("click", () => {
-    loadEditor();
-  });
+  if(splitscreenButton) {
+    const groupStudySelector =
+      document.getElementById("groupStudySelector") as HTMLSelectElement ;
 
-  function loadEditor() {
-    const docId = groupStudySelector.value as T.DocId;
-    if (docId == currentDocId) return;
-    if(currentDocId) {
-      ws.send({ tag: "StopListenToDoc", contents: docId });
-    }
-    currentDocId = docId;
-    ws.send({ tag: "ListenToDoc", contents: docId });
-  }
+    let editor : Editor.P215Editor = null;
+    let currentDocId : T.DocId;
 
-  ws.addEventListener("DocListenStart", (ev : WS.DocListenStartEvent) => {
-    const doc = ev.document;
-    console.log(doc);
-    if(editor) {
-      editor.removeEditor();
-    }
-    editor = new Editor.P215Editor({
-      initDoc: doc,
-      editable: false,
-      remoteThings: null,
+    groupStudySelector.addEventListener("input", () => {
+      loadEditor();
     });
-    const elem = document.getElementById("sideBySideEditor");
-    editor.addEditor(elem);
 
-  });
+      splitscreenButton.addEventListener("click", () => {
+        loadEditor();
+      });
 
-  ws.addEventListener("DocUpdated", (ev : WS.DocUpdatedEvent) => {
-    editor.dispatchSteps(ev.update);
-  });
+    function loadEditor() {
+      const docId = groupStudySelector.value as T.DocId;
+      if (docId == currentDocId) return;
+      if(currentDocId) {
+        ws.send({ tag: "StopListenToDoc", contents: docId });
+      }
+      currentDocId = docId;
+      ws.send({ tag: "ListenToDoc", contents: docId });
+    }
+
+    ws.addEventListener("DocListenStart", (ev : WS.DocListenStartEvent) => {
+      const doc = ev.document;
+      console.log(doc);
+      if(editor) {
+        editor.removeEditor();
+      }
+      editor = new Editor.P215Editor({
+        initDoc: doc,
+        editable: false,
+        remoteThings: null,
+      });
+      const elem = document.getElementById("sideBySideEditor");
+      editor.addEditor(elem);
+
+    });
+
+    ws.addEventListener("DocUpdated", (ev : WS.DocUpdatedEvent) => {
+      editor.dispatchSteps(ev.update);
+    });
+  }
 }
