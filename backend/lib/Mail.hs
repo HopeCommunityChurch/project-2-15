a102d4fccc8c126eb1e8a7a5a2d8e1d336e0f93f
jonny <jonny.covert@gmail.com>
2024-12-05 13:29:44 -0500

    Log when sending emails.

--- backend/lib/Mail.hs
+++ backend/lib/Mail.hs
@@ -3,7 +3,7 @@ module Mail where
 import Network.HaskellNet.SMTP qualified as Smtp
 import Network.Socket (PortNumber)
 import GHC.Records (HasField)
-import Network.Mail.Mime (Mail)
+import Network.Mail.Mime (Mail(..))
 
 data Smtp = MkSmtp
   { host :: String
@@ -17,9 +17,11 @@ sendMail
   :: ( HasSmtp env
      , MonadUnliftIO m
      , MonadReader env m
+     , MonadLogger m
      )
   => Mail
   -> m ()
 sendMail m = do
+  logInfo $ "sending email to: " <> show m.mailTo
   smtp <- asks (.smtp)
   liftIO $ Smtp.doSMTPPort smtp.host smtp.port $ Smtp.sendMail m
