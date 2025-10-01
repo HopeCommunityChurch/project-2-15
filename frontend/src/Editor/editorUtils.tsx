daa527af5b92e792a6b8d01b371cecb59e1d81c7
jonny <jonny.covert@gmail.com>
2025-10-01 00:19:22 -0400

    Re-ordering now works.

--- frontend/src/Editor/editorUtils.tsx
+++ frontend/src/Editor/editorUtils.tsx
@@ -399,3 +399,52 @@ export const deleteSection = (
     dispatch(tr);
   }
 }
+
+// Use the order of the array to say what the new order is
+export const reorderStudyBlock = (
+  pos: number,
+  changes : Array<Node>
+) => (
+  state: EditorState,
+  dispatch?: (tr: Transaction) => void
+) => {
+  let sectionNode : Node = null;
+  let sectionPos = null;
+  console.log(pos);
+  state.doc.nodesBetween(pos, pos, (node, pos) => {
+    if (node.type.name === "section") {
+      sectionNode = node;
+      sectionPos = pos;
+      return true;
+    }
+  });
+  console.log(sectionNode);
+  if (sectionNode === null) {
+    console.error("unable to get section for editing study block");
+    return;
+  }
+  let studyBlockNode = null
+  sectionNode.descendants((node, pos, parent) => {
+    if(parent.type.name === "section" && node.type.name === "studyBlocks") {
+      studyBlockNode = {node, pos};
+      return false;
+    }
+    return false;
+  });
+  console.log(studyBlockNode);
+
+  let tr = state.tr;
+  let delPos = studyBlockNode.pos + sectionPos;
+  console.log(delPos);
+  tr.deleteRange(delPos+1, delPos + studyBlockNode.node.nodeSize-1);
+
+  let currentPos = delPos + 1;
+  changes.forEach((node) => {
+    tr.insert(currentPos, node);
+    currentPos += node.nodeSize;
+  });
+
+  dispatch(tr)
+  return true;
+
+};
--- frontend/src/StudyBlockEditor.ts
+++ frontend/src/StudyBlockEditor.ts
@@ -1,21 +1,84 @@
-import * as WS from "./WebsocketTypes"
-import * as T from "./Types"
-import { Slice, Node, Mark, Fragment } from "prosemirror-model";
+import { Node } from "prosemirror-model";
 import * as Editor from "./Editor/Editor"
+import * as EditorUtil from "./Editor/editorUtils"
 
 document.addEventListener("editorAttached", (ev : Editor.EditorAttached) => {
   const editor = ev.editor;
-  const container = document.getElementById("studyBlockEditor");
+  const container = document.getElementById("studyBlockEditorContent");
+  let currentStudyBlock : Editor.EditorStudyBlocksEdit = null;
+
+  document.getElementById("studyBlockCancel").addEventListener("click", () => {
+    window.toggleModal("#studyBlockEditor");
+  });
+
   editor.addEventListener("studyblocks-edit", (ev : Editor.EditorStudyBlocksEdit) => {
     container.innerHTML = "";
     window.toggleModal("#studyBlockEditor");
     const studyBlocks = ev.studyBlocks;
-    studyBlocks.forEach( ({node}, _, index) => {
+    currentStudyBlock = ev
+
+    studyBlocks.forEach( ({node}, index) => {
       const div = document.createElement("div");
+      div.className = "studyBlock";
+
+      div.addEventListener("mousedown", (e: MouseEvent) => {
+        if(e.button == 0) {
+          document.addEventListener("mousemove", mousemove);
+          document.addEventListener("mouseup", mouseup);
+        }
+      });
+
+
+      function mouseup () {
+        document.removeEventListener("mouseup", mouseup);
+        document.removeEventListener("mousemove", mousemove);
+        div.classList.remove("moving");
+        let currentIndex = Number(div.getAttribute("currentIndex"));
+      };
+
+      function mousemove (e : MouseEvent) {
+        div.classList.add("moving");
+        const rec = div.getBoundingClientRect();
+        if(e.clientY > rec.bottom) {
+          moveDown();
+        } else if (e.clientY < rec.top) {
+          moveUp();
+        }
+      };
+
+      function moveDown () {
+        const next = div.nextElementSibling;
+        if(next) {
+          let ncurrentIndex = Number(next.getAttribute("currentIndex"));
+          ncurrentIndex --;
+          next.setAttribute("currentIndex", ncurrentIndex + "");
+
+          div.parentElement.insertBefore(next, div);
+          let currentIndex = Number(div.getAttribute("currentIndex"));
+          currentIndex ++;
+          div.setAttribute("currentIndex", currentIndex + "");
+        }
+      }
+
+      function moveUp () {
+        const previous = div.previousElementSibling;
+        if(previous) {
+          let pcurrentIndex = Number(previous.getAttribute("currentIndex"));
+          pcurrentIndex ++;
+          previous.setAttribute("currentIndex", pcurrentIndex + "");
+
+          div.parentElement.insertBefore(div, previous);
+          let currentIndex = Number(div.getAttribute("currentIndex"));
+          currentIndex --;
+          div.setAttribute("currentIndex", currentIndex + "");
+        }
+      }
+
+
+
 
       div.setAttribute("originalIndex", index + "");
       div.setAttribute("currentIndex", index + "");
-      div.setAttribute("oldIndex", index + "");
 
       let name = "";
       if (node.type.name == "questions") {
@@ -32,5 +95,22 @@ document.addEventListener("editorAttached", (ev : Editor.EditorAttached) => {
       container.appendChild(div);
     });
   });
+
+  document.getElementById("studyBlockUpdate").addEventListener("click", () => {
+    const container = document.getElementById("studyBlockEditorContent");
+    if (currentStudyBlock === null) return;
+    let newPosistions = [];
+    container.querySelectorAll("*").forEach( (child) => {
+      const currentIndex = Number(child.getAttribute("currentIndex"));
+      const originalIndex = Number(child.getAttribute("originalIndex"));
+      newPosistions[currentIndex] = currentStudyBlock.studyBlocks[originalIndex].node;
+    });
+    editor.applyDispatch(EditorUtil.reorderStudyBlock(
+      currentStudyBlock.studyBlockPos,
+      newPosistions
+    ));
+    window.toggleModal("#studyBlockEditor");
+  });
 });
+
 
