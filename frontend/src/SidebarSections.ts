df0f75aea70df0f350aa6785bfb2fee241345cff
James Hendrie <jhendrie25@gmail.com>
2026-02-18 23:51:42 -0500

    Clean up typo, dead code, and unused variable

--- frontend/src/SidebarSections.ts
+++ frontend/src/SidebarSections.ts
@@ -60,9 +60,7 @@ function createSectionHeader(editor : Editor.P215Editor, index : number) {
   section.appendChild(sectionText);
 
   const remove = document.createElement("div");
-  remove.innerHTML = "х";
-  // const img = document.createElement("img");
-  // img.src = "/static/img/x.svg";
+  remove.innerHTML = "x";
   remove.className = "remove";
   remove.addEventListener("click", (e) => {
     e.stopPropagation();
